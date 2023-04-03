dfe0a033a438787110edad2b512342f05d56274a
kronos <bradleykitione@gmail.com>
2023-04-03 22:58:33 +1000

    made things connect hori and vert but only at 1 point

--- main.cpp
+++ main.cpp
@@ -11,12 +11,14 @@ using std::vector;
 #define WIDTH 10
 // colours
 #define CURS_COLOUR "\033[38;5;49m"
+#define BLACK_FG "\033[38;5;235m"
 #define RESET "\033[m"
 // clr screen
 #define clear() std::cout << "\033c";
 
+// global variables
 char grid[HEIGHT][WIDTH];
-int cursor_y = HEIGHT / 2, cursor_x = WIDTH / 2;
+unsigned int cursor_y = HEIGHT / 2, cursor_x = WIDTH / 2;
 bool connect_dots = false;
 bool removed_x = false;
 
@@ -29,13 +31,13 @@ void initGrid() {
 }
 void printGrid() {
     clear();
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
+    for (unsigned int i = 0; i < HEIGHT; i++) {
+        for (unsigned int j = 0; j < WIDTH; j++) {
             if (cursor_y == i && cursor_x == j) {
                 std::cout << CURS_COLOUR << grid[i][j] << RESET << "   ";
-            } else {
-                std::cout << grid[i][j] << "   ";
-            }
+            } else if (grid[i][j] == '|' || grid[i][j] == '-' || grid[i][j] == 'X'){
+                std::cout << BLACK_FG << grid[i][j] << RESET << "   ";
+            } else { std::cout << grid[i][j] << "   "; }
         }
         std::cout << std::endl;
     } 
@@ -64,7 +66,7 @@ void moveCursor() {
             grid[cursor_y][cursor_x] = 'X';
             break;
         case BACKSPACE:
-            removed_x = true;
+            removed_x = true; // if a 'X' was erased
             grid[cursor_y][cursor_x] = '.';
             break;
         case ENTER:
@@ -77,7 +79,7 @@ void moveCursor() {
 }
 // save x and y positions of 'X' plots
 struct Positions {
-    int y_dot, x_dot;
+    unsigned int y_dot, x_dot;
 };
 // ensures the findPositions func doesn't store an existing pairs
 bool repeatedPositions(vector<Positions> &pos) {
@@ -104,7 +106,6 @@ void findPositions(vector<Positions> &pos) {
     if (grid[cursor_y][cursor_x] == 'X') {
         if (!repeatedPositions(pos)) { 
             // saves x and y coords in a new struct for every 'X'
-            // constructor inside 'push_back()'
             pos.push_back(Positions({cursor_y , cursor_x}));
         }
     } else if (removed_x) {
@@ -115,26 +116,63 @@ void findPositions(vector<Positions> &pos) {
 void printPositions(vector<Positions> &pos) {
     vector<Positions>::iterator i = pos.begin();
     for (; i < pos.end(); i++) {
-        std::cout << "Y: " << (*i).y_dot << '\t';
-        std::cout << "X: " << (*i).x_dot << std::endl;
+        // it just lets me see the position my cursor is on :)
+        if (cursor_y == (*i).y_dot && cursor_x == (*i).x_dot) {
+            std::cout << CURS_COLOUR << "Y: " << (*i).y_dot << '\t';
+            std::cout << "X: " << (*i).x_dot << RESET << std::endl;
+        } else {
+            std::cout << "Y: " << (*i).y_dot << '\t';
+            std::cout << "X: " << (*i).x_dot << std::endl;
+        }
+
     }
 }
 
-void connectDots() {
-    // horizontal
+void connectHorizontal(Positions head, Positions tail) {
+    unsigned x_begin = head.x_dot + 1, x_end = tail.x_dot;
+    for (; x_begin < x_end; x_begin++) {
+        grid[head.y_dot][x_begin] = '#';
+    }
+}
+
+void connectVertical(Positions head, Positions tail) {
+    unsigned y_begin = head.y_dot + 1, y_end = tail.y_dot;
+    for (; y_begin < y_end; y_begin++) {
+        grid[y_begin][head.x_dot] = '#';
+    }
     
 }
 
+void findConnects(vector<Positions> &pos) {
+    vector<Positions>::iterator i = pos.begin();
+    vector<Positions>::iterator j = pos.begin();
+    for (; i != pos.end(); ++i) {
+        for (; j != pos.end(); ++j) {
+            if ((*i).y_dot == (*j).y_dot) { // points are on the same row
+                if ((*i).x_dot < (*j).x_dot) {
+                    connectHorizontal(*i, *j);
+                } else { connectHorizontal(*j, *i); }
+            }
+            if ((*i).x_dot == (*j).x_dot) { // points are on the same column
+                if ((*i).y_dot < (*j).y_dot) {
+                    connectVertical(*i, *j);
+                } else { connectVertical(*j, *i); }
+            }
+        }
+    }
+}
 
 int main() {
     vector<Positions> pos;
     initGrid();
     while (!connect_dots) {
-        printGrid();
         findPositions(pos);
+        printGrid();
         printPositions(pos); // just to test
         moveCursor();
     }
+    findConnects(pos);
+    printGrid();
 
     return EXIT_SUCCESS;
 }
